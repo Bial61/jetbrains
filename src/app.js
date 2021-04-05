325da8bba41ee50bcc56c9168c2c335dfde8c1b4
bial61 <iambilalahmad61@gmail.com>
2021-04-04 23:08:36 -0700

    first

---
+++ src/app.js
@@ -0,0 +1,70 @@
+const path = require('path')
+const express = require('express')
+const hbs= require ('hbs')
+
+
+
+const {convertWordFiles} = require("convert-multiple-files");
+// const {Powerpoint,Word} =require("pdf-officegen");
+
+const app = express()
+const port = process.env.PORT || 3000
+
+//set path of views and static assests
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname,'../templates/views')
+const partialPath= path.join(__dirname,'../templates/partials') 
+
+//Setup for handelbars engine and views
+app.set('views', viewsPath);
+app.set('view engine', 'hbs')
+hbs.registerPartials(partialPath)
+
+//Use static assests like css,client side js, images
+app.use(express.static(publicDirectoryPath))
+
+
+app.get('/doc',async (req,res)=>
+{
+
+
+    try 
+    {
+        await convertWordFiles(path.join(__dirname, '../hi.doc'), 'pdf', path.join(__dirname,'../'));
+        res.sendFile(path.join(__dirname,'../hi.pdf'),null,(err)=>
+        {
+            if(err)
+            {
+                throw new Error("Error");
+            }else
+            {
+                 //
+            }
+        })
+
+    } catch (error) 
+    {
+          res.send({
+              error:"Error"
+          });    
+    }
+    
+
+})
+
+// app.get('/word', async (req,res)=>
+// {
+ 
+//     const paths =path.join(__dirname,'../hi.pdf')
+//     const p = new Word();
+//     await p.convertFromPdf([paths] ,path.join(__dirname,'../'), (err, result) => {
+//         //Do something with the result (filepath to output) 
+    
+//       })
+
+// })
+
+
+app.listen(port, () => {
+    console.log('Server is up on port 3000.')
+})
