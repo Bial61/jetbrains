ee42b27081a804e9caa41527c4343f7eb30cde86
bial61 <iambilalahmad61@gmail.com>
2021-04-06 01:48:52 -0700

    abnd

--- src/app.js
+++ src/app.js
@@ -65,31 +65,33 @@ const upload = multer({
 
 app.post('/doc',upload.single('file'),async (req,res)=>
 {
+
+    res.send(re.file);
    
-    try 
-    {
-        await convertWordFiles(path.join(__dirname,`../uploads/${req.file.originalname}`), 'pdf', path.join(__dirname,'../converted'));
-        var nameFile = req.file.originalname.split('.');
-        nameFile[0]=nameFile[0]+'.pdf';
-        console.log(nameFile[0]);
-        res.sendFile(path.join(__dirname,`../converted/${nameFile[0]}`),null,(err)=>
-        {
-            if(err)
-            {
-                throw new Error("Fail to send File.")
-            }else
-            {
-                 //
-            }
-        })
+    // try 
+    // {
+    //     await convertWordFiles(path.join(__dirname,`../uploads/${req.file.originalname}`), 'pdf', path.join(__dirname,'../converted'));
+    //     var nameFile = req.file.originalname.split('.');
+    //     nameFile[0]=nameFile[0]+'.pdf';
+    //     console.log(nameFile[0]);
+    //     res.sendFile(path.join(__dirname,`../converted/${nameFile[0]}`),null,(err)=>
+    //     {
+    //         if(err)
+    //         {
+    //             throw new Error("Fail to send File.")
+    //         }else
+    //         {
+    //              //
+    //         }
+    //     })
 
 
-    } catch (error) 
-    {
-          res.send({
-              error
-          });    
-    } 
+    // } catch (error) 
+    // {
+    //       res.send({
+    //           error
+    //       });    
+    // } 
     
 
 
